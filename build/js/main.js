30869777f708b420878c3dfbf84e709b9f1cbafc
Gleb Leus <lv.gleb44@gmail.com>
2023-11-01 01:03:03 +0300

    where-to-buy-page

--- build/js/main.js
+++ build/js/main.js
@@ -138,64 +138,18 @@ if (document.querySelector(".basket-aside") && window.screen.width >= 768) {
    });
 }
 
-ymaps.ready(function () {
-   var myMap = new ymaps.Map(
-         "map",
-         {
-            center: [55.751574, 37.573856],
-            zoom: 9,
-         },
-         {
-            searchControlProvider: "yandex#search",
-         }
-      ),
-      // Создаём макет содержимого.
-      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-         '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-      ),
-      myPlacemark = new ymaps.Placemark(
-         myMap.getCenter(),
-         {
-            hintContent: "Собственный значок метки",
-            balloonContent: "Это красивая метка",
-         },
-         {
-            // Опции.
-            // Необходимо указать данный тип макета.
-            iconLayout: "default#image",
-            // Своё изображение иконки метки.
-            iconImageHref: "images/myIcon.gif",
-            // Размеры метки.
-            iconImageSize: [30, 42],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
-            iconImageOffset: [-5, -38],
-         }
-      ),
-      myPlacemarkWithContent = new ymaps.Placemark(
-         [55.661574, 37.573856],
-         {
-            hintContent: "Собственный значок метки с контентом",
-            balloonContent: "А эта — новогодняя",
-            iconContent: "12",
-         },
-         {
-            // Опции.
-            // Необходимо указать данный тип макета.
-            iconLayout: "default#imageWithContent",
-            // Своё изображение иконки метки.
-            iconImageHref: "images/ball.png",
-            // Размеры метки.
-            iconImageSize: [48, 48],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
-            iconImageOffset: [-24, -24],
-            // Смещение слоя с содержимым относительно слоя с картинкой.
-            iconContentOffset: [15, 15],
-            // Макет содержимого.
-            iconContentLayout: MyIconContentLayout,
-         }
-      );
+// Переключение городов на странице "Где купить"
+if (document.querySelector(".buy-map")) {
+   const tabs = document.querySelectorAll(".buy-map__tab-btn"),
+      tabsContainer = document.querySelector(".buy-map__tabs");
 
-   myMap.geoObjects.add(myPlacemark).add(myPlacemarkWithContent);
-});
+   tabsContainer.addEventListener("click", (e) => {
+      tabs.forEach((tab) => {
+         tab.classList.remove("active");
+      });
+
+      if (e.target.classList.contains("buy-map__tab-btn")) {
+         e.target.classList.add("active");
+      }
+   });
+}
